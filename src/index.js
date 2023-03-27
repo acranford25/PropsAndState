52037437643321c5b81a53af8e8880f563215465
Andrew Cranford <acranford25@gmail.com>
2023-03-27 12:34:05 -0400

    got some circles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 // write your Color component here
+const Color = (props) => {
+  return (
+    <div className ={props.color} />
+  )
+}
 
 const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('');
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color='red' selectColor={selectColor} />
+        <Color color='blue' selectColor={selectColor}/>
+        <Color color='green' selectColor={selectColor}/>
+      </div>
     </div>
   );
+}
+
+const selectColor = (colorName) => {
+  setSelectedColor(colorName);
 }
 
 ReactDOM.render(
